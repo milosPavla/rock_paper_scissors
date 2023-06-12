358bca66d927f58547dacc56361b6d86c2e90a61
Milos0213 <milospavla002@gmail.com>
2023-06-12 19:16:15 +0200

    Completed final function and done some improvement First, I changed the output of playRound() and now it returns winner which is crucial for core function and enables program to quickly distinguish which score variable to increment. Also, I deleted previous one cycle at the bottom of code as it was no more needed. Mistake was made as I placed section of game() which is used to print out the winner in the FOR-cycle so it started showing winner and looser every round.

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ let getComputerChoice = () => {
 let getPlayerChoice = () => prompt("Type in your choice (rock, paper, scissors): ");
 
 /* One cicle of game checking if inputs are same, if not, then it checks if inputs are so that
-player has won. Everything else that isn't one of these conditions above are considered defeat */
+player has won. Everything else that isn't one of these conditions above are considered defeat
+Output of this function returns string of winner  */
 let playRound = (player, computer) => {
     if (player == computer) {
         alert("It's a tie! Play on!");
@@ -24,17 +25,51 @@ let playRound = (player, computer) => {
     else if ( (player == 'rock' && computer == 'scissors') || 
         (player == 'paper' && computer == 'rock') || 
         (player == 'scissors' && computer == 'paper') ) {
-        alert('Player has won!');
+        return 'player';
     } 
     else {
-        alert('Computer has won!');
+        return 'computer';
     }
 }
+
+/* Core function of game has two main variables which are used to store score of both contributors.
+Game goes through five cycles in which player chooses between 3 options and tries to beat computer.
+Every cycle, variables get new values from choice functions and compares them in IF function.
+As playRound() returns winner of cycle, program compares winner string with 'player' or 'computer'
+and according to that, points to score are added. In the end, winner is shown on alert window.
+ */
+let game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i ++) {
+        let player = getPlayerChoice();
+        let computer = getComputerChoice();
+
+        console.log(player);
+        console.log(computer);
+
+        if(playRound(player, computer) == 'player') {
+            playerScore++;
+        } else if(playRound(player, computer) == 'computer') {
+            computerScore++;
+        }
+        
+        console.log('Player score: ' + playerScore);
+        console.log('Computer score: ' + computerScore);
+    }
+
+    if(playerScore > computerScore) {
+        alert('Player has won!');
+    } else if(playerScore < computerScore) {
+        alert('You have been defeated!');
+    } else {
+        alert('It is a tie score! Restart this page to play again.')
+    }
+}
+
+/* Calling game function */
+game();
     
-/* Storing functions outputs to variables, writing it to console and calling one round of game */
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-console.log(computerSelection);
-console.log(playerSelection);
-playRound(playerSelection, computerSelection);
+
 
