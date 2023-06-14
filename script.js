cdb63ed1ad84feabe5a38d024496407edd97eb1d
Milos0213 <milospavla002@gmail.com>
2023-06-14 22:40:29 +0200

    HTML and CSS implemented along with script file Added all html and css code needed for now. Been stuck event listeners for certain items for a bit, but in short future expceting it to be polished. Plan is to make some smooth transition animations for swiping cards and their selection along with some new features like points counter and implementing more rounds instead of one phase game. Problem for now is that as I implemented images instead of text on cards, event listener is not recognizing id's of container, but instead it finds id's of images which does not meet requirements in functions. So for that, there's always a tie game.

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-/* Function expression which uses Math.random to generate number between 0 and 2
-flooring it to clean number. According to value of that number stored in index variable, we have if
-statement that returns certain string  */
-let getComputerChoice = () => {
+function getComputerChoice() {
     let index = Math.floor(Math.random() * 3);
-    if(index === 0) {
-        return "rock";
-    } else if(index === 1) {
-        return "paper";
-    } else {
-        return "scissors";
+    switch (index) {
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+        default:
+            break;
     }
 }
 
-/* Function that gets input by user using prompt window */
-let getPlayerChoice = () => prompt("Type in your choice (rock, paper, scissors): ");
-
-/* One cicle of game checking if inputs are same, if not, then it checks if inputs are so that
-player has won. Everything else that isn't one of these conditions above are considered defeat
-Output of this function returns string of winner  */
-let playRound = (player, computer) => {
+function compareInputs(player, computer) {
     if (player == computer) {
         return 'tie';
     } 
@@ -32,44 +29,38 @@ let playRound = (player, computer) => {
     }
 }
 
-/* Core function of game has two main variables which are used to store score of both contributors.
-Game goes through five cycles in which player chooses between 3 options and tries to beat computer.
-Every cycle, variables get new values from choice functions and compares them in IF function.
-As playRound() returns winner of cycle, program compares winner string with 'player' or 'computer'
-and according to that, points to score are added. In the end, winner is shown on alert window.
- */
-let game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
+function startGame(e) {
+    playerChoice = e.target.id;
+    computerChoice = getComputerChoice();
 
-    for(let i = 0; i < 5; i ++) {
-        let player = getPlayerChoice();
-        let computer = getComputerChoice();
+    let computerOutput = document.querySelector('.computer-output');
+    computerOutput.innerHTML = `<img src="images/${computerChoice}.png"></img>`;
 
-        console.log(player);
-        console.log(computer);
+    console.log('pc:' + playerChoice);
+    console.log('cc:' + computerChoice);
 
-        if(playRound(player, computer) == 'player') {
-            playerScore++;
-        } else if(playRound(player, computer) == 'computer') {
-            computerScore++;
+    let winner = compareInputs(playerChoice, computerChoice);
+
+    setTimeout(() => {
+        if(winner == 'player') {
+            alert('You have won!');
+        } else if(winner == 'computer') {
+            alert('You lose! :C');
+        } else {
+            alert(`It's a tie, try again.`);
         }
-        
-        console.log('Player score: ' + playerScore);
-        console.log('Computer score: ' + computerScore);
-    }
+    }, 500);
 
-    if(playerScore > computerScore) {
-        alert('Player has won!');
-    } else if(playerScore < computerScore) {
-        alert('You have been defeated!');
-    } else {
-        alert('It is a tie score! Restart this page to play again.')
-    }
 }
 
-/* Calling game function */
-game();
-    
+const cards = document.querySelectorAll('.card');
 
+cards.forEach(this.addEventListener('click', startGame));
+
+
+// const startButton = document.querySelector('.start-button');
+// startButton.addEventListener('click', startGame);
+
+
+game();
 
