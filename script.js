1d581b6fb4acdcb3d3870b16d6d2ee44d5ad0e38
Milos0213 <milospavla002@gmail.com>
2023-06-16 01:57:04 +0200

    Added score system, player icons and footer Score incrementation is determined by winner output. When incremented, scoreboard zooms in a bit and returns to its original scale. Function also test if score has reached its limit, if so, windows pops up and opacity changes. After that, a new round starts and score counters reset to zero. New icons implemented as profile icons along with score points counter. Footer added with some information.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getComputerChoice() {   // Returns random choice by computer
     let index = Math.floor(Math.random() * 3);
     switch (index) {
         case 0:
@@ -29,10 +29,29 @@ function compareInputs(player, computer) { // Returns winner by comparing choice
     }
 }
 
-function startGame(e) {
-    // e.stopPropagation()
+function checkScore() {     // Sets current score to scoreboard, tests if score has hit five, if so, triggers pop window with desired message and resets score to zero
+    document.querySelector('#player-score').innerText = playerScore;
+    document.querySelector('#computer-score').innerText = computerScore;
 
-    playerChoice = this.id;  // changed from e.target.id ==> this.id because 'this' cathes all clicks inside of a div,
+    if(playerScore > 4 || computerScore > 4) {
+        document.querySelector('.info-box').classList.add('show');
+        document.querySelector('.info-box').innerHTML = `<h1>${playerScore > 4 ? 'You have won!' : 'You have been defeated!'}</h1>`
+        document.querySelector('body').style.opacity = '0.6';
+
+        playerScore = 0, computerScore = 0;
+
+        setTimeout(() => {  // Removes added attributes aftet 1 second
+            document.querySelector('.info-box').classList.remove('show');
+            document.querySelector('body').style.opacity = '1';
+            return
+        }, 1000);
+
+        return
+    }
+}
+
+function startGame() {
+    playerChoice = this.id;  // Changed from e.target.id ==> this.id because 'this' catches all clicks inside of a div,
                              // but e.target cathes only clicks exactly on that target and not on child clicks.
     computerChoice = getComputerChoice();
 
@@ -42,24 +61,31 @@ function startGame(e) {
     console.log('pc:' + playerChoice);
     console.log('cc:' + computerChoice);
 
-    let winner = compareInputs(playerChoice, computerChoice);
+    let winner = compareInputs(playerChoice, computerChoice);   // winner specified by function output
 
-    setTimeout(() => {
-        if(winner == 'player') {
-            alert('You have won!');
-        } else if(winner == 'computer') {
-            alert('You lose! :C');
-        } else {
-            alert(`It's a tie, try again.`);
-        }
-    }, 500);
+    if(winner == 'player') {
+        playerScore++;
 
+        document.querySelector('#player-profile .score-container').classList.add('zoomed');  // Adds scale effect on score incremented
+        setTimeout(() => {
+            document.querySelector('#player-profile .score-container').classList.remove('zoomed'); // Removes after 1 second
+        }, 1000);
+
+    } else if(winner == 'computer') {
+        computerScore++;
+
+        document.querySelector('#computer-profile .score-container').classList.add('zoomed');
+        setTimeout(() => {
+            document.querySelector('#computer-profile .score-container').classList.remove('zoomed');
+        }, 1000);
+    } else {
+
+    }
+
+    checkScore();
 }
 
+let playerScore = 0, computerScore = 0;  // Implementation of score variables
+
 const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => card.addEventListener('click', startGame, {
-    // capture: false
-})); 
-
-
+cards.forEach(card => card.addEventListener('click', startGame));  // Adding listeners on each card
